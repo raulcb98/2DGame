b8cf84cd1fd547a224ff8e20ecc5f612c45c79cb
raulcb98 <raul.castilla215@alu.uhu.es>
2020-03-24 19:52:45 +0100

    C0002 - Shot mechanism
     I0005 - Shot implemented.
     I0006 - Shot animation implemented.
     I0007 - Enemy death animation implemented.
    
     E0005 - Bullets aren't destroyed if don't impact.
     E0006 - Death animation has event to finish it.

---
+++ Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public float damage = 40f;
+    public GameObject bulletImpactAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponentInChildren<Enemy>();
+
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(bulletImpactAnimation, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 100f;
+    public GameObject deathEffect;
+
+    // Update is called once per frame
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
