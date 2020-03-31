f04daceec65c4b15a581022a14772e5db5927b48
raulcb98 <raul.castilla215@alu.uhu.es>
2020-03-31 19:16:26 +0200

    C0004 - Frog Jump
    I0010 - Add a new scene to test frog jump.
    I0011 - Add Frog jump.
    
    E0007 - Frog can jump in the middle of the air.
    E0008 - Frog jump can be ilimited.

---
+++ Project/Assets/Jump.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity = 5;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    
+    Rigidbody2D rb;
+    Animator animator;
+
+    const bool RIGHT = true;
+    const bool LEFT = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal"); // -1 = left, 0 = nothing, +1 = right
+        if(x != 0)
+        {
+            rb.velocity = new Vector2(x, 1) * jumpVelocity;
+        }
+
+        jumpPhysics();
+        jumpAnimation();
+    }
+
+
+    void jumpPhysics()
+    {
+
+        if (rb.velocity.y < 0)
+        {
+            animator.SetBool("falling", true);
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+
+    void jumpAnimation()
+    {
+        if(rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        
+        if (rb.velocity.y != 0)
+        {
+            animator.SetBool("jumping", true);
+            if(rb.velocity.y < 0)
+            {
+                animator.SetBool("falling", true);
+            }
+        } else
+        {
+            animator.SetBool("jumping", false);
+            animator.SetBool("falling", false);
+        }
+    }
+}
--- Project/Assets/Scripts/IdleBehaviour.cs
+++ Project/Assets/Scripts/IdleBehaviour.cs
@@ -8,6 +8,7 @@ public class IdleBehaviour : StateMachineBehaviour
     Transform player;
     Rigidbody2D rb;
     public float speed = 2.5f;
+    int jumpTimes = 0;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -22,8 +23,13 @@ public class IdleBehaviour : StateMachineBehaviour
         //Vector2 target = new Vector2(player.position.x, rb.position.y);
        // Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         //rb.MovePosition(newPos);
+ 
+        
 
-        rb.AddForce(new Vector2(2, 0));
+       rb.velocity += Vector2.up * Physics2D.gravity.y * 20.5f * Time.fixedDeltaTime;
+       jumpTimes++;
+        
+  
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
