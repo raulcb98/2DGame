350ff669413d66b76a30b560b45f04b214440e95
raulcb98 <raul.castilla215@alu.uhu.es>
2020-04-04 18:52:56 +0200

    C0015 - Scripts reorganization
     I0026 - Items have two separeted scripts for functionality and
             for destroy animation.
     I0027 - Main camera has a script to search the player and follow him.

---
+++ Project/Assets/Scripts/Cameras/Main_Camera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        GetComponent<Cinemachine.CinemachineVirtualCamera>().Follow = player;
+    }
+}
